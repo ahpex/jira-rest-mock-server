6a2f51767b6088e9de518cbed4110e03f8705b40
Michael Pietsch <github@mpietsch.com>
2024-12-19 11:54:52 +0100

    removed obsolete code in search route

--- src/server/routes/search.ts
+++ src/server/routes/search.ts
@@ -1,23 +1,10 @@
 import express from "express";
-import fs from "fs";
-import path from "path";
 import { faker } from "../../faker";
 
 const router = express.Router();
 
 router.get("/", (req: express.Request, res: express.Response) => {
-    const delay = req.body.delay || 0;
-    const total: number = req.body.total;
-
-    setTimeout(() => {
-        const filePath = path.join(__dirname, "../responses/search.json");
-        fs.readFile(filePath, "utf8", (err, data) => {
-            if (err) {
-                return res.status(500).json({ error: "Failed to read file" });
-            }
-            res.json(faker.jira.search({ total }));
-        });
-    }, delay);
+    res.json(faker.jira.search());
 });
 
 export default router;
